29f6f2ec0e2aa9af7312cdbcfd0e8d6b2b5b97f6
Ilona <venusflytrapd.d@gmail.com>
2023-09-05 15:23:48 +0300

    Appetizer Added

--- Assets/UIController.cs
+++ Assets/UIController.cs
@@ -3,38 +3,45 @@ using UnityEngine.UIElements;
 
 public class UIController : MonoBehaviour
 {
-    private VisualElement Container_PopUp;
+    private VisualElement Container1;
+    private VisualElement Container1_PopUp;
     private VisualElement Scrim_PopUp;
     private VisualElement Window_PopUp;
-    private VisualElement Button_Open_PopUp;
-    private VisualElement Button_Close_PopUp;
-    private VisualElement Button_Open_Shop;
+    private VisualElement Button_OpenPopUp;
+    private VisualElement Button_ClosePopUp;
+    private VisualElement Button_OpenShop;
 
     void Start()
     {
         var root = GetComponent<UIDocument>().rootVisualElement;
-
-        Container_PopUp = root.Q("Container_PopUp");
+        Container1= root.Query("Container1");
+        Container1_PopUp = root.Q("Container1_PopUp");
         Scrim_PopUp = root.Q("Scrim_PopUp");
         Window_PopUp = root.Q("Window_PopUp");
-        Button_Open_PopUp = root.Q("Button_OpenPopUp");
-        Button_Close_PopUp = root.Q("Button_ClosePopUp");
+        Button_OpenPopUp = root.Q("Button_OpenPopUp");
+        Button_ClosePopUp = root.Q("Button_ClosePopUp");
+        Button_OpenShop = root.Query("Button_OpenShop").First();
 
-        Button_Open_PopUp.RegisterCallback<ClickEvent>(OnOpenPopUpButtonClicked);
-        Button_Close_PopUp.RegisterCallback<ClickEvent>(OnClosePopUpButtonClicked);
+        Button_OpenPopUp.RegisterCallback<ClickEvent>(OnOpenPopUpButtonClicked);
+        Button_ClosePopUp.RegisterCallback<ClickEvent>(OnClosePopUpButtonClicked);
+        Button_OpenShop.RegisterCallback<ClickEvent>(OnOpenShopButtonClicked);
     }  
 
     private void OnOpenPopUpButtonClicked(ClickEvent ev)
     {
-        Container_PopUp.style.display = DisplayStyle.Flex;
+        Container1_PopUp.style.display = DisplayStyle.Flex;
         Window_PopUp.AddToClassList("popUpWindow-up");
         Scrim_PopUp.AddToClassList("scrim-fadedIn");
 
     }
     private void OnClosePopUpButtonClicked(ClickEvent ev)
     {
-        Container_PopUp.style.display = DisplayStyle.None;
+        Container1_PopUp.style.display = DisplayStyle.None;
         Window_PopUp.RemoveFromClassList("popUpWindow-up");
         Scrim_PopUp.RemoveFromClassList("scrim-fadedIn");
+    }
+    private void OnOpenShopButtonClicked(ClickEvent ev)
+    {
+        Container1.style.display = DisplayStyle.None;
     }
 }
