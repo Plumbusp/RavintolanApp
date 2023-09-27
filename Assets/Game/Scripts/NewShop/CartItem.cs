95049853f4ab35d422712e4062864c43078ffbc4
Ilona <venusflytrapd.d@gmail.com>
2023-09-27 12:59:10 +0300

    Start of the work on cart

---
+++ Assets/Game/Scripts/NewShop/CartItem.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CartItem : MonoBehaviour
+{
+    public Image image;
+    public string foodName;
+    public int amount;
+}
--- Assets/Game/Scripts/NewShop/CartManager.cs
+++ Assets/Game/Scripts/NewShop/CartManager.cs
@@ -1,8 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
-public class CartManager : MonoBehaviour
+public class CartManager 
 {
-    
+    private List<FoodVariant> itemsInCart = new List<FoodVariant>();
+    public void AddToCart(FoodVariant item)
+    {
+        itemsInCart.Add(item);
+    }
+    public void RemoveFromCart(FoodVariant item)
+    {
+        itemsInCart.Remove(item);
+    }
+    public void InstanciateAllItemsInCart(List<CartItem> cartItems)
+    {
+        List<FoodVariant> cleanItemsInCart = itemsInCart.GroupBy(n => );
+        foreach (CartItem cartItem in cartItems)
+        {
+            ca
+        }
+    }
 }
