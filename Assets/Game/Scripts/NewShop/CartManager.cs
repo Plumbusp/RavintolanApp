b7eae82b1ed1518b89dcdb8bbaa9538a6d372967
Ilona <venusflytrapd.d@gmail.com>
2023-09-27 13:00:57 +0300

    Start of the work on cart

--- Assets/Game/Scripts/NewShop/CartManager.cs
+++ Assets/Game/Scripts/NewShop/CartManager.cs
@@ -4,5 +4,25 @@ using UnityEngine;
 
 public class CartManager : MonoBehaviour
 {
+<<<<<<< Updated upstream
     
+=======
+    private List<FoodVariant> itemsInCart = new List<FoodVariant>();
+    public void AddToCart(FoodVariant item)
+    {
+        itemsInCart.Add(item);
+    }
+    public void RemoveFromCart(FoodVariant item)
+    {
+        itemsInCart.Remove(item);
+    }
+    public void InstanciateAllItemsInCart(List<CartItem> cartItems)
+    {
+        //List<FoodVariant> cleanItemsInCart = itemsInCart.GroupBy(n => );
+        foreach (CartItem cartItem in cartItems)
+        {
+            
+        }
+    }
+>>>>>>> Stashed changes
 }
